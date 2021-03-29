a7ed88aff82b76ecb8157ae09cc47c1253093eaa
andrescorredorrincon <felipecorredor9010@gmail.com>
2021-03-28 23:13:22 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+// Variables y constantes
+
+let nombre;
+let apellido;
+const edad = 30;
+nombre = "Andres Felipe";
+apellido = "Corredor";
+console.log(nombre, apellido, edad);
+
+
+// Tipos de datos
+let Nombre = `Andres Felipe Corredor Rincon`; //String
+let Edad = 30; //number - intenger - int;
+let altura = 1.73; //Double - float
+let profesor = true; // Boleano
+let informacion = [Nombre, Edad, altura, profesor]; //Array
+console.log(informacion);
+// console.log(informacion[2]);
+let baso = {
+	material : "porcelan",
+	diametro :  60,
+	lleno : false
+}; // Object
+console.log(baso);
+// console.log(baso.diametro);
+
+
+
+
+
+
